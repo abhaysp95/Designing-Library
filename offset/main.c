52b147a99cdc94f98c1e1578c4367203b784b663
coolabhays <abhaysp9955@gmail.com>
2022-04-09 19:52:35 +0530

    feat: show how to get offset of the attributes by macro
        wrote "offset" macro which will return the offset from the
        provided data structure object its the provided field name

---
+++ offset/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct emp_ {
+	char name[32];
+	unsigned int salary;
+	char designation[32];
+	unsigned int emp_id;
+} emp_t;
+
+#define offset(struct_name, field_name) \
+	(unsigned int)&((struct_name *)0)->field_name
+
+/** -> comes above & in operator precedence */
+
+int main(int argc, char **argv) {
+	int name_offset = offset(emp_t, name);
+	int salary_offset = offset(emp_t, salary);
+	int designation_offset = offset(emp_t, designation);
+	int emp_id_offset = offset(emp_t, emp_id);
+	printf("%d\n", name_offset);
+	printf("%d\n", salary_offset);
+	printf("%d\n", designation_offset);
+	printf("%d\n", emp_id_offset);
+	return 0;
+}
